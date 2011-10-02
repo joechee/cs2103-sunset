744f14fc8dbbb935f7c202147b9d459273c37ec2
Joe Chee <joe.chee@gmail.com>
2011-10-02 16:55:39 +0000

    *Killer feature JOTD is up.

---
+++ src/cs2103/aug11/t11j2/fin/jotd/Joke.java
@@ -0,0 +1,42 @@
+package cs2103.aug11.t11j2.fin.jotd;
+
+import java.io.IOException;
+import java.util.*;
+
+import cs2103.aug11.t11j2.fin.storage.Serializer;
+
+public class Joke {
+	List<String> jokeList ;
+	Serializer jokeSerial;
+	@SuppressWarnings("unchecked")
+	public Joke() throws IOException {
+		jokeList = new ArrayList<String>();
+		jokeSerial = new Serializer();
+		//System.out.println(jokeSerial.unserialize("Joke.yaml").get(1));
+		List<Object> serialList = jokeSerial.unserialize("Joke.yaml");
+		Iterator<Object> serialListIterator = serialList.iterator();
+		while (serialListIterator.hasNext()) {
+			jokeList.add((String) serialListIterator.next());
+		}
+		
+		
+	}
+	
+
+	public String generate() {
+		Random RNG = new Random();
+		return jokeList.get(RNG.nextInt(jokeList.size()));
+	}
+		
+	public void addJoke(String joke) throws IOException {
+		jokeList.add(joke);
+		saveJokes();
+	}
+	
+	public void saveJokes() throws IOException {
+		jokeSerial.serialize(new ArrayList<Object>(jokeList),"Joke.yaml");
+	}
+		
+			
+		
+}
