d8c5ba6ddcd93535090ee18a59a0c6e4b12dce94
Joe Chee <joe.chee@gmail.com>
2011-10-06 08:41:41 +0000

    * Deleted FinBootstrapper.java and UI.java

--- src/cs2103/aug11/t11j2/fin/ui/UI.java
+++
@@ -1,9 +0,0 @@
-package cs2103.aug11.t11j2.fin.ui;
-
-import cs2103.aug11.t11j2.fin.parser.CommandResult;
-
-interface UI {
-	void mainLoop();
-	// Actually, I don't think updateContext needs to be a UI interface method. It should not be publicly accessible.
-	// void updateContext(CommandResult feedback);
-}
