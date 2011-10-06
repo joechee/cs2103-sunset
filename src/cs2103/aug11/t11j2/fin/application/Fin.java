227429051cd00e8a88c21ea5296f6d29c957859f
Joe Chee <joe.chee@gmail.com>
2011-10-06 04:50:29 +0000

    * Updated Fin.java (bootstrapper)

--- src/cs2103/aug11/t11j2/fin/application/Fin.java
+++ src/cs2103/aug11/t11j2/fin/application/Fin.java
@@ -1,8 +1,8 @@
 package cs2103.aug11.t11j2.fin.application;
 
 
-import cs2103.aug11.t11j2.fin.application.FinConstants.UI;
-import cs2103.aug11.t11j2.fin.ui.CLI;
+
+import cs2103.aug11.t11j2.fin.ui.*;
 
 /* Usage:
  * Fin -<UI> <filename>
@@ -11,87 +11,53 @@ import cs2103.aug11.t11j2.fin.ui.CLI;
 
 public class Fin {
 
-	private static UI ui = FinConstants.DEFAULT_UI;
-	private static String calendarFile = FinConstants.DEFAULT_FILE;
-	
-	private static class InputException extends Exception {
-
-		private static final long serialVersionUID = 1L;
-		public InputException(String e) {
-			super(e);	
-		}
-		
-	}
-	
-
+	private static final String fileExtension = ".yaml";
 	
 	public static void main(String[] args) {
 		try {
 			parseArgs(args);
-			FinApplication.INSTANCE.loadEnvironment(calendarFile);
-			LoadUI();
-		} catch (InputException e) {
+		} catch (IllegalArgumentException e) {
 			System.out.print(e.getMessage());
 		}
 
 	}
 	
-	private static void LoadUI() throws InputException {
-		switch (ui) {
-		case CLI:
-			CLI.mainLoop();
-			return;
-		default:
-			throw new InputException("Type of UI not recognised");
-				
-		/*
-		case GUI:
-			GUI.mainLoop();
-			return;
-			
-		case WeirdUI:
-			WeirdUI.mainLoop();
-			return;
-		
-		case JokeUI:
-			JokeUI.mainLoop();
-			return;
-		*/
-		}
-		
-	}
 
-	public static void parseArgs(String[] args) throws InputException {
-	
-		
+
+	private static void parseArgs(String[] args) {
+		IUserInterface UI = FinConstants.DEFAULT_UI;
 		for (String i: args) {
 			if (i.toCharArray()[0] == '-') {
-				parseUI(i);
+				UI = parseUI(i);
 			} else {
-				calendarFile = i;
+				FinApplication.INSTANCE.loadEnvironment(checkFilename(i));
 			}
 		}
-		
-		
-		
+		UI.mainLoop();
 	}
 
-	private static void parseUI(String UIString) throws InputException {
-		
+
+
+	private static String checkFilename(String i) throws IllegalArgumentException {
+		if (i == null) {
+			throw new IllegalArgumentException("Invalid Input!");
+		} else if (i.split(".").length == 0) {
+			return i+fileExtension;
+		}
+		return null;
+	}
+
+	private static IUserInterface parseUI(String UIString) throws IllegalArgumentException {
 		if (UIString.equals("-")) {
-			throw new InputException ("No UI provided!");
+			throw new IllegalArgumentException ("No UI provided!");
 		}
 		UIString = UIString.toUpperCase().split("-")[1];
 		if (UIString.equals("CLI")){
-			ui = UI.CLI;
-		} else if (UIString.equals("GUI")){
-			ui = UI.GUI;
+			return (IUserInterface) new CLI();
+		} else if (UIString.equals("NUI")){
+			return (IUserInterface) null;
 		} else {
-			throw new InputException ("UI not recognised!");
+			throw new IllegalArgumentException ("UI not recognised!");
 		}
-
-		
-		
 	}
-
 }
