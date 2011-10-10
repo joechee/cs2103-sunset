de8b688667693de2e60ffa3b15129eb32a27a7e4
Joe Chee <joe.chee@gmail.com>
2011-10-10 15:40:40 +0000

    *Updated HelpCommandParser

--- src/cs2103/aug11/t11j2/fin/parser/HelpCommandHandler.java
+++ src/cs2103/aug11/t11j2/fin/parser/HelpCommandHandler.java
@@ -22,6 +22,7 @@ public class HelpCommandHandler implements CommandParser.ICommandHandler {
 				add("?");
 				add("help");
 				add("h");
+				add("f1");
 			}
 		};
 	}
@@ -40,28 +41,63 @@ public class HelpCommandHandler implements CommandParser.ICommandHandler {
 	}
 
 	private CommandResult showHelpFor(String arguments) throws FinProductionException {
-		if (arguments.equals("edit")) {
+		if (arguments.equals("add")) {
 			return new CommandResult(this, "",
-					CommandResult.RenderType.String, "I need help for edit!");			
+					CommandResult.RenderType.String, "add <task>\t\t\tAdds <task> to your task list\n");			
 		} else if(arguments.equals("show")) {
 			return new CommandResult(this, "",
-					CommandResult.RenderType.String, "I need help for show!");						
-		}
+					CommandResult.RenderType.String, "show <filters>\t\t\tShows the task list with <filters>. Examples of <filters> can be \"tasks due on Friday\"\n");						
+		} else if(arguments.equals("search")) {
+			return new CommandResult(this, "",
+					CommandResult.RenderType.String, "search <patterns>\t\tSearch the tasks containing the (all) the <patterns>\n");
+		} else if(arguments.equals("delete")) {
+			return new CommandResult(this, "",
+					CommandResult.RenderType.String, "delete <task number>\t\tDeletes a task from your tasklist\n");
+		} else if(arguments.equals("tag")) {
+			return new CommandResult(this, "",
+					CommandResult.RenderType.String, "tag <task number> <tag>\t\tTags a <task> with <tag>\n");
+		} else if(arguments.equals("untag")) {
+			return new CommandResult(this, "",
+					CommandResult.RenderType.String, "untag <task number> <tag>\tUntags a <task> with <tag>\n");
+		} else if(arguments.equals("fin")) {
+			return new CommandResult(this, "",
+					CommandResult.RenderType.String, "fin <task number>\t\tMarks a task as completed\n");
+		} else if(arguments.equals("unfin")) {
+			return new CommandResult(this, "",
+					CommandResult.RenderType.String, "unfin <task number>\t\tUnmarks the completed task\n");
+		} else if(arguments.equals("help")) {
+			return new CommandResult(this, "",
+					CommandResult.RenderType.String, "Are you joking?");
+		} else if(arguments.equals("important")) {
+			return new CommandResult(this, "",
+					CommandResult.RenderType.String, "important\t\t\t\tMarks a task as important\n");
+		} else if(arguments.equals("unimportant")) {
+			return new CommandResult(this, "",
+					CommandResult.RenderType.String, "unimportant\t\t\t\tUnmarks an important task\n");
+		} else if(arguments.equals("edit")) {
+			return new CommandResult(this, "",
+					CommandResult.RenderType.String, "edit <task number> to <new task> \t\tChanges a task to the new task");
+		} 
 		
 		return showDefaultHelp();			
 		
 	}
 
 	private CommandResult showDefaultHelp() throws FinProductionException {
-		final String help = "Fin. is a ToDo manager that will allow you to keep track of your daily tasks the way you want it to be tracked.\n\n" +
-				"add <task>\t\tAdds <task> to your task list\n" +
-				"show <filters>\t\tShows the task list with <filters>. Examples of <filters> can be \"tasks due on Friday\"\n" +
-				"search <patterns>\tSearch the tasks containing the (all) the <patterns>\n" +
-				"delete <task>\t\tDeletes a task from your tasklist\n" +
-				"tag <task> <tag>\tTags a <task> with <tag>\n" +
-				"untag <task> <tag>\tUntags a <task> with <tag>\n" +
-				"fin <task>\tMarks a task as completed\n" +
-				"unfin <task>\t";
+		final String help = 
+			"Fin. is a ToDo manager that will allow you to keep track of your daily tasks the way you want it to be tracked.\n\n" +
+				"add <task>\t\t\t\tAdds <task> to your task list\n" +
+				"show <filters>\t\t\t\tShows the task list with <filters>. Examples of <filters> can be \"tasks due on Friday\"\n" +
+				"search <patterns>\t\t\tSearch the tasks containing the (all) the <patterns>\n" +
+				"delete <task number>\t\t\tDeletes a task from your tasklist\n" +
+				"tag <task number> <tag>\t\t\tTags a <task> with <tag>\n" +
+				"untag <task number> <tag>\t\tUntags a <task> with <tag>\n" +
+				"fin <task number>\t\t\tMarks a task as completed\n" +
+				"unfin <task number>\t\t\tUnmarks the completed task\n" +
+				"help\t\t\t\t\tBrings you to this page\n"+
+				"important\t\t\t\tMarks a task as important\n" +
+				"unimportant\t\t\t\tUnmarks an important task\n" +
+				"edit <task number> to <new task> \tChanges a task to the new task";
 		return new CommandResult(this, "",
 				CommandResult.RenderType.String, help);
 	}
